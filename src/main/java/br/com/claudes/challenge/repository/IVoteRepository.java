2ee7ccf6c7e115dbfc36171d82d0fa78c2c1d19d
Claudes Gomes <claudesferreira99@gmail.com>
2021-06-25 01:17:48 -0300

    fix: adjust in the querys of the votes

--- src/main/java/br/com/claudes/challenge/repository/IVoteRepository.java
+++ src/main/java/br/com/claudes/challenge/repository/IVoteRepository.java
@@ -11,13 +11,13 @@ import java.util.Optional;
 @Repository
 public interface IVoteRepository extends JpaRepository<Vote, Long> {
 
-  @Query(value = "SELECT * FROM desafio.tb_voto " +
+  @Query(value = "SELECT * FROM tb_voto " +
           "WHERE document = ?1  AND session_id = ?2",
           nativeQuery = true)
   public Optional<Vote> findByDocumentAndSessionId(String document, Long sessionId);
 
-  @Query(value = "SELECT * FROM desafio.tb_voto voto " +
-          "inner join desafio.tb_sessao sessao on sessao.session_id = voto.session_id " +
+  @Query(value = "SELECT * FROM tb_voto voto " +
+          "inner join tb_sessao sessao on sessao.session_id = voto.session_id " +
           "WHERE sessao.session_id = ?1 ",
   nativeQuery = true)
   public List<Vote> findyBySessionId(Long sessionId);
